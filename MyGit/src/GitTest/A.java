86b4b6d3d751856533b59a3be10a5da66297b8e7
박천보 <@PARK-C-B>
2023-12-14 23:12:28 +0900

    change in master branch

--- MyGit/src/GitTest/A.java
+++ MyGit/src/GitTest/A.java
@@ -3,5 +3,7 @@ package GitTest;
 public class A {
 	public void main() {
 		System.out.println("This is A.java");
+		System.out.println("A.java is changing in master!");
+		System.out.println("New change in A.java!");
 	}
 }
