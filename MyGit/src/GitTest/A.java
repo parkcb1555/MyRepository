cd840d8f577c4cd4e82c9e1ed2ef04f3886a0b2e
박천보 <@PARK-C-B>
2023-12-14 22:24:49 +0900

    derB branc

--- MyGit/src/GitTest/A.java
+++ MyGit/src/GitTest/A.java
@@ -3,5 +3,6 @@ package GitTest;
 public class A {
 	public void main() {
 		System.out.println("This is A.java");
+		System.out.println("derB is working in A.java");
 	}
 }
--- MyGit/src/GitTest/B.java
+++ MyGit/src/GitTest/B.java
@@ -3,5 +3,6 @@ package GitTest;
 public class B {
 	public void main() {
 		System.out.println("This is B.java");
+		System.out.println("derB is working in B.java");
 	}
 }
